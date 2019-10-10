d36668c9cd839432bac20ff0e2e4a410d20dbbed
sschoor163 <steven_Schoor@yahoo.com>
2019-10-09 21:01:34 -0500

    fuck this shit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ITodo} from '../interfaces/Itodo';
+import {TodoService} from '../services/todo.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My-Trello-Clone';
+  displayedColumns: string[] = ['Backlog', 'Working', 'Complete'];
+  constructor(private TodoService: TodoService){}
+  
+  
+
+  working = this.TodoService.getTodoItems("Working");
+  backlog = this.TodoService.getTodoItems("Backlog");
+  complete = this.TodoService.getTodoItems("Complete");
+  dataSource = new MatTableDataSource<ITodo[]>([this.backlog, this.working, this.complete ]);
+  ngOnInit(){
+    
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './todo/todo.component';
+import { TodoService } from 'src/services/todo.service';
+import {MatTableModule} from '@angular/material'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
